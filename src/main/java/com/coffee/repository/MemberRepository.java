8092ed783ee3e430ef1514f0e3bed936e5636df8
jewangkka007 <jewangkka007@naver.com>
2025-09-19 16:05:16 +0900

    2025/09/19

--- src/main/java/com/coffee/repository/MemberRepository.java
+++ src/main/java/com/coffee/repository/MemberRepository.java
@@ -7,4 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // 이전의 Dao 역할
 // JpaRepository<엔터티이름, 해당엔터티의기본키변수의타입>
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    // findByEmail를 JPA에서는 '쿼리 메소드'라고 부릅니다.
+    // 이메일 정보를 이용하여 해당 회원이 존재하는지 체크합니다.
+    Member findByEmail(String email);
 }
